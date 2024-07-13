73b02f69abe6c6da833ebf0029327deed3c087b9
Sahil-B07 <objectorientedguy23@gmail.com>
2024-07-14 00:12:12 +0530

    update models

--- customer_service/models.py
+++ customer_service/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 
 class CustomerAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=15)
 
     def __str__(self):
         return self.user.username
