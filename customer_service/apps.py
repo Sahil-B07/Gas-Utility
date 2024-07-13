4c7360622216251e1420496ee00fa37f11eba8ff
Sahil-B07 <objectorientedguy23@gmail.com>
2024-07-14 00:11:46 +0530

    add signals.py

--- customer_service/apps.py
+++ customer_service/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CustomerServiceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "customer_service"
+
+    def ready(self):
+        import customer_service.signals
